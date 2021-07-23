bac3a7691be37319c94348b96900486926fe3556
kome <yoneda886@gmail.com>
2021-07-23 19:00:57 +0900

    教室を追加

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 var app = new Vue({
     el: '#app',
     data: {
-    message: '',
-    search:'',
-    cities: [],
+        message: '',
+        search: '',
+        cities: [],
+
+        searchGenre: 'class',
     },
     methods: {
         async getCities() {
-        var url = 'list-2.json'
-        await axios.get(url).then(x => { this.cities = x.data })
+            var url = 'list-2.json'
+            await axios.get(url).then(x => { this.cities = x.data })
         }
     },
     mounted() {
         this.getCities()
     },
     computed: {
-        search_tantou(){
-        　return this.cities.filter(value => {
-            return value.tantou.includes(this.search)
-        　})
-    　}
+        search_tantou() {
+            return this.cities.filter(value => {
+                return value.kamoku.includes(this.search)
+            })
+        }
     },
-    
-    
+
+
 })
